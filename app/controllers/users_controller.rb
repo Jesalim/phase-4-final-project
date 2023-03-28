8f2caafe72d6f361c7710c36a4e75f45c9f3798f
nick <mwauranicholas10@gmail.com>
2023-03-28 22:12:00 +0300

    added code in controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
-end
+    # Use rescue_from to handle ActiveRecord::RecordNotFound exception
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  
+    # Skip authorize method before create and show actions
+    skip_before_action :authorize, only: [:create, :show]
+  
+    # Remove format from wrapped parameters
+    wrap_parameters format: []
+  
+    def index
+      # Render all users in JSON format
+      render json: User.all
+    end
+  
+    def show
+      # Find the user by session ID
+      @user = User.find_by(id: session[:user_id])
+  
+      # Render the user in JSON format
+      render json: @user
+    end
+  
+    def create
+      # Create a new user with permitted parameters
+      new_user = User.create!(new_user_params)
+  
+      # Check if the new user is valid and render a JSON response
+      if new_user.valid?
+        render json: new_user, status: :created
+      else
+        render json: { errors: new_user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    # Define permitted parameters for creating a new user
+    def new_user_params
+      params.permit(:first_name, :last_name, :email, :gender, :age, :password, :password_confirmation)
+    end
+  
+    # Define record not found error handling method
+    def record_not_found
+      render json: { errors: ['User not found'] }, status: :not_found
+    end
+  end
+  
---
+++ test/controllers/bookflight_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BookflightControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
