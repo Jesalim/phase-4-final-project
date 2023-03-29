7970e8add0a9f0f3e7326c68e742e61dd1f9a493
nick <mwauranicholas10@gmail.com>
2023-03-29 09:39:25 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  resources :flights, only: [:index, :destroy, :update, :create, :show]
+  # resources :users, only: [:index, :create, :show]
+  # get '/me', to: 'users#show'
+
+  # resources :users, only: [:show] do
+  #   # nested resource for flights
+  #   resources :flights, only: [:show, :index, :create]
+  # end
+
+
+
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
